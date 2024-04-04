a19604f0fb1e8e3afff698c86f024cfd7251b879
Rafif Priyo <muhammad.rafif01@ui.ac.id>
2024-04-04 11:17:19 +0700

    Completing the basic tutorial

--- scripts/Area2D.gd
+++ scripts/Area2D.gd
@@ -4,5 +4,11 @@ export(String) var sceneName = "Level 1"
 
 
 func _on_Area_Trigger_body_entered(body):
+	var current_scene = get_tree().get_current_scene().get_name()
 	if body.get_name() == "Player":
-		get_tree().change_scene(str("res://scenes/" + sceneName + ".tscn"))
+		if current_scene == sceneName:
+			Global.lives -=1
+		if (Global.lives == 0):
+			get_tree().change_scene(str("res://scenes/Game Over.tscn"))
+		else:
+			get_tree().change_scene(str("res://scenes/" + sceneName + ".tscn"))
