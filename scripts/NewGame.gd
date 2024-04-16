36493ba498a3bc4ab40857e3f92186951f4f3af5
Rafif Priyo <muhammad.rafif01@ui.ac.id>
2024-04-16 07:52:27 +0700

    Implement individual exercise
    - Implement main menu button in game over scene
    - Implement stage select UI

--- scripts/NewGame.gd
+++ scripts/NewGame.gd
@@ -3,5 +3,6 @@ extends LinkButton
 export(String) var scene_to_load
 
 func _on_New_Game_pressed():
+	Global.reset_lives()
 	get_tree().change_scene(str("res://scenes/" + scene_to_load + ".tscn"))
 
--- scripts/global.gd
+++ scripts/global.gd
@@ -1,3 +1,6 @@
 extends Node
 
 var lives = 3
+
+func reset_lives():
+	lives = 3
